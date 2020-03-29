d3624dd539324e232b317312d1646b9421b90f0e
mac <yangliuliu@qiutianji.com>
2020-03-29 17:57:09 +0800

    quthor : liuliu desible : haha

--- src/main/java/com/source/controller/Testcontroller.java
+++ src/main/java/com/source/controller/Testcontroller.java
@@ -1,4 +1,18 @@
 package com.source.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class Testcontroller {
+
+    @GetMapping("hello")
+    public String show(){
+
+        return "hello world";
+    }
+
+
 }
